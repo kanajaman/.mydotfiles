ca742ec9e71e292871a7aefa6b3ad777afc153e5
kanajaman <kanajalab@gmail.com>
2019-12-21 10:53:36 +0900

    touch *.local

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ln -s $SCRIPT_DIR/.bashrc ~/.bashrc
 ln -s $SCRIPT_DIR/.vimrc ~/.vimrc
 ln -s $SCRIPT_DIR/.ideavimrc ~/.ideavimrc
+touch ~/.vimrc.local
+touch ~/.bashrc.local
+touch ~/.ideavimrc.local
